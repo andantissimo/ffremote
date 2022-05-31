ca32840b9663421c2660b206fdd82a9c94658f73
MALU <contact@andantissimo.jp>
2022-05-31 13:14:52 +0900

    fix race condition

--- ffremote/System.Diagnostics/ProcessExtensions.cs
+++ ffremote/System.Diagnostics/ProcessExtensions.cs
@@ -4,18 +4,22 @@ internal static class ProcessExtensions
 {
     public static async Task<int> StartAsync(this Process process, CancellationToken cancellationToken = default)
     {
-        var exited = new TaskCompletionSource<int>();
+        var tcs = new TaskCompletionSource<int>();
         process.EnableRaisingEvents = true;
-        process.Exited += (_, _) => exited.TrySetResult(process.ExitCode);
+        process.Exited += Exited;
         process.Start();
         if (process.HasExited)
             return process.ExitCode;
+
         using var _ = cancellationToken.Register(() =>
         {
+            process.Exited -= Exited;
             if (process.Id != default)
                 process.Kill(true);
-            exited.TrySetCanceled(cancellationToken);
+            tcs.TrySetCanceled(cancellationToken);
         });
-        return await exited.Task.ConfigureAwait(false);
+        return await tcs.Task.ConfigureAwait(false);
+
+        void Exited(object? sender, EventArgs e) => tcs.TrySetResult(process.ExitCode);
     }
 }
